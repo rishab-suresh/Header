fa8924c562ae90bac4f0af284583c77b4292d81e
rishab-suresh <56022095+rishab-suresh@users.noreply.github.com>
2022-05-04 13:49:57 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Navbar/navbar.css'
+const Navbar = () => {
+  return (
+    <div className='header'>
+        <img src='C:\Users\sures\header\header\public\img\Group 3661.png'></img>
+    </div>
+  )
+}
+
+export default Navbar
